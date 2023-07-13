44385cc3ed8862ce6b14d23fcaa618d517781430
Brankonymous <brankogrbic01@gmail.com>
2023-07-13 17:36:22 +0200

    Add ballance

--- src/WalletPage.js
+++ src/WalletPage.js
@@ -3,6 +3,28 @@ import React, { useState, useEffect } from 'react';
 
 import Web3 from 'web3';
 
+const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+// The minimum ABI to get ERC20 Token balance
+let minABI = [
+    // balanceOf
+    {
+      "constant":true,
+      "inputs":[{"name":"_owner","type":"address"}],
+      "name":"balanceOf",
+      "outputs":[{"name":"balance","type":"uint256"}],
+      "type":"function"
+    },
+    // decimals
+    {
+      "constant":true,
+      "inputs":[],
+      "name":"decimals",
+      "outputs":[{"name":"","type":"uint8"}],
+      "type":"function"
+    }
+  ];
+
 function WalletPage() {
     const [balanceInETH, setBalanceInETH] = useState(0);
     const [balanceInDAI, setBalanceInDAI] = useState(0);
@@ -15,9 +37,20 @@ function WalletPage() {
 
     useEffect(() => {
         (async () => {
-            const balance = await web3.eth.getBalance(accountAddress);
-            setBalanceInETH(web3.utils.fromWei(balance, 'ether'));
+            const ETHBalance = await web3.eth.getBalance(accountAddress);
+            setBalanceInETH(web3.utils.fromWei(ETHBalance, 'ether'));
             
+            let DAIContract = web3.eth.contract(minABI).at(DAIAddress);
+            setBalanceInDAI(
+                    DAIContract.balanceOf(accountAddress, (error, balance) => {
+                    // Get decimals
+                    DAIContract.decimals((error, decimals) => {
+                    // calculate a balance
+                    balance = balance.div(10**decimals);
+                    console.log(balance.toString());
+                    });
+                })
+            );
 
             console.log(balanceInETH);
         })()
@@ -27,6 +60,7 @@ function WalletPage() {
         <div>
             <h1>Address: {accountAddress}</h1>
             <h3>ETH: {balanceInETH}</h3>
+            <h3>DAI: {balanceInDAI}</h3>
         </div>
     );
 }
